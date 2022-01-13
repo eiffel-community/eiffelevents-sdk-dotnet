f3014fcf8557479d1d6db8c861df62a719608e6c
FadyKamil <engfadykamil.2014@gmail.com>
2022-01-13 18:00:45 +0200

    adding examples

--- examples/EiffelClient.PublisherOne/Models/Edition-Lyon/Client.cs
+++ examples/EiffelClient.PublisherOne/Models/Edition-Lyon/Client.cs
@@ -39,7 +39,7 @@ namespace EiffelClient.PublisherOne.Models.Edition_Lyon
                 nameof(EiffelActivityCanceledEvent) => ActivityCanceled.GetEvent() as T,
                 nameof(EiffelActivityFinishedEvent) => ActivityFinished.GetEvent() as T,
                 nameof(EiffelArtifactCreatedEvent) => ArtifactCreated.GetEvent() as T,
-                // nameof(EiffelArtifactReusedEvent) => ArtifactReused.GetEvent() as T,
+                nameof(EiffelArtifactReusedEvent) => ArtifactReused.GetEvent() as T,
                 nameof(EiffelArtifactPublishedEvent) => ArtifactPublished.GetEvent() as T,
                 // nameof(EiffelConfidenceLevelModifiedEvent) => ConfidenceLevelModified.GetEvent() as T,
                 // nameof(EiffelEnvironmentDefinedEvent) => EnvironmentDefined.GetEvent() as T,
