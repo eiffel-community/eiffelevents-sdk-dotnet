4cc97c69373f8fa7e0433350a69a49dd8975c268
Ahmed Abou Elfotouh <ahmed.fotouh@live.com>
2022-01-17 19:06:42 +0200

    Add EiffelTestCaseTriggeredEvent Lyon edition examples.

--- examples/EiffelClient.PublisherOne/Models/Edition-Lyon/Client.cs
+++ examples/EiffelClient.PublisherOne/Models/Edition-Lyon/Client.cs
@@ -47,7 +47,7 @@ namespace EiffelClient.PublisherOne.Models.Edition_Lyon
                 nameof(EiffelCompositionDefinedEvent) => CompositionDefined.GetEvent() as T,
                 nameof(EiffelFlowContextDefinedEvent) => FlowContextDefined.GetEvent() as T,
                 nameof(EiffelSourceChangeSubmittedEvent) => SourceChangeSubmitted.GetEvent() as T,
-                // nameof(EiffelTestCaseTriggeredEvent) => TestCaseTriggered.GetEvent() as T,
+                nameof(EiffelTestCaseTriggeredEvent) => TestCaseTriggered.GetEvent() as T,
                 // nameof(EiffelTestSuiteStartedEvent) => TestSuiteStarted.GetEvent() as T,
                 // nameof(EiffelTestSuiteFinishedEvent) => TestSuiteFinished.GetEvent() as T,
                 // nameof(EiffelTestCaseCanceledEvent) => TestCaseCanceled.GetEvent() as T,
